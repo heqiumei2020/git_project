15198f3de4926df5e79d50e4d491d5e13f6f2a89
heqiumei2020 <1318854229@qq.com>
2020-06-09 15:13:03 +0800

    9号提交

---
+++ git-project/src/com/git/dao/UserDao.java
@@ -0,0 +1,24 @@
+package com.git.dao;
+
+import java.util.List;
+
+public class UserDao {
+
+
+		public void addUser(int id,String username,String password) {
+			
+		}
+		public void deledeUser(int id) {
+			
+		}
+	
+		public void updeteUser(int id,String newUsername,String newPassword) {
+			
+		}
+		public List<Object> scanUser (int pageIndex,int pageSize){
+			return null;
+		}
+		
+	
+
+}
